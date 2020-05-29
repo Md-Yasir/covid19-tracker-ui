29a28f28794390ac2a62d8e3b51397856fc2e306
Yasir <mohdyasir231990@gmail.com>
2020-05-29 19:43:22 +0530

    ..

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const app = express();
  
 // Serve only the static files form the dist directory
 // Replace the '/dist/covid19telangana'
-app.use(express.static(__dirname + '/dist/covid19telangana')); 
+app.use(express.static(__dirname + '/dist')); 
  
 app.get('*', function(req,res) {
   // Replace the '/dist/covid19telangana/index.html'
-  res.sendFile(path.join(__dirname + '/dist/covid19telangana/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
